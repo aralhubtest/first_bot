2399daae9113ac4fe814bb8bea7135017a87df5c
meloman <melomanovmeloman@gmail.com>
2025-08-04 10:37:25 +0500

    add photo

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
 import asyncio
 import os
 from dotenv import load_dotenv
+import logging
+
 
 from aiogram import Bot, Dispatcher, F
 from aiogram.filters import Command
 from aiogram.types import Message
 
 
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
 load_dotenv()
 
-TOKEN = os.getenv('BOT_TOKEN') # 8420270855:AAF7Ral1S9xtCd-nqQgCUgHKIO8MwKP2k_A
+TOKEN = os.getenv("BOT_TOKEN")  # 8420270855:AAF7Ral1S9xtCd-nqQgCUgHKIO8MwKP2k_A
 
 dp = Dispatcher()
 
 
+async def sum_numbers(a: int, b: int):
+    return a + b
 
 
 # Command handler
@@ -21,14 +28,25 @@ dp = Dispatcher()
 async def command_start_handler(message: Message):
     await message.reply(f"Hello ! {message.from_user.first_name}")
 
-@dp.message(Command('help'))
+
+@dp.message(Command("help"))
 async def help_information(message: Message):
-    await message.answer(f'username = {message.from_user.username}')
+    await message.answer_photo('AgACAgIAAxkBAANtaJBDsYT9o7upw9i2LS9FAwM8bDsAArDxMRsnMoFIjjwd50VUF7EBAAMCAANzAAM2BA')
 
-@dp.message(F.text == 'test')
+@dp.message(F.text == "test") # if text == test
 async def magicflter(message: Message):
-    await message.answer(f'username = {message.from_user.username}')
+    await message.answer(f"chat_id = {message.chat.id}")
 
+
+@dp.message(F.photo)
+async def hello(soobsheine: Message):
+    await soobsheine.reply(f'id = {soobsheine.photo[-1].file_id} - size = {soobsheine.photo[-1].file_size} uniqid = {soobsheine.photo[-1].file_unique_id}')
+    await soobsheine.reply(f'id = {soobsheine.photo[0].file_id} - size = {soobsheine.photo[0].file_size} uniqid = {soobsheine.photo[0].file_unique_id}')
+
+
+@dp.message() #if True
+async def echo(message: Message):
+    await message.answer(message.text[::-1])
 
 
 # Run the bot
@@ -40,5 +58,3 @@ async def main() -> None:
 
 if __name__ == "__main__":
     asyncio.run(main())
-
-
