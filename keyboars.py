7e0edc2d64d4b751969e97c806ed2770ef045ce0
meloman <melomanovmeloman@gmail.com>
2025-08-15 10:21:53 +0500

    gg

--- keyboars.py
+++ keyboars.py
@@ -1,15 +1,13 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 # Создание кнопок
-button_1 = KeyboardButton(text="Кнопка 1")
-button_2 = KeyboardButton(text="Кнопка 2")
-button_3 = KeyboardButton(text="Кнопка 3")
+button_1 = KeyboardButton(text="Mexanika")
+button_2 = KeyboardButton(text="Automat")
 
 # Создание клавиатуры с одной кнопкой в ряду
 keyboard_single_row = ReplyKeyboardMarkup(
-    keyboard=[[button_1], [button_2], [button_3]],
+    keyboard=[[button_1], [button_2]],
     resize_keyboard=True, # Изменяет размер клавиатуры под контент
     one_time_keyboard=True, # Скрывает клавиатуру после использования
-    input_field_placeholder="Выберите действие..." # Подсказка в поле ввода
-
+    #input_field_placeholder="Выберите действие..." # Подсказка в поле ввода
 )
--- main.py
+++ main.py
@@ -2,14 +2,32 @@ import asyncio
 import os
 from dotenv import load_dotenv
 import logging
-
-
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram import Bot, Dispatcher, F
 from aiogram.filters import Command
-from aiogram.types import Message, FSInputFile
+from aiogram.types import Message, FSInputFile, chat
 from generate_qr import generate as qr_gen
-# from keyboars import keyboard_single_row
+from keyboars import keyboard_single_row
 from test_api import get_weather
+from aiogram.fsm.state import State, StatesGroup
+
+
+class FormCity(StatesGroup):
+    city = State() # Состояние для запроса имени
+
+class FormUrl(StatesGroup):
+    url = State()
+
+
+class FormCar(StatesGroup):
+    marka = State()
+    year = State()
+    country = State()
+    color = State()
+    gear_box = State()
+
+
 
 
 logging.basicConfig(level=logging.INFO)
@@ -17,9 +35,11 @@ logging.basicConfig(level=logging.INFO)
 load_dotenv()
 
 TOKEN = os.getenv("BOT_TOKEN")  # 8420270855:AAF7Ral1S9xtCd-nqQgCUgHKIO8MwKP2k_A
-
-dp = Dispatcher()
-
+# Инициализация хранилища в памяти
+storage = MemoryStorage()
+# При инициализации Dispatcher передайте ему хранилище
+dp = Dispatcher(storage=storage)
+bot = Bot(token=TOKEN)
 
 # Command handler
 @dp.message(Command("start"))
@@ -28,14 +48,12 @@ async def command_start_handler(message: Message):
         text=f"Hello ! {message.from_user.first_name}", reply_markup=None)
 
 
-@dp.message(F.text == 'Кнопка 1')
-async def generate(message: Message):
-    output_file_path = qr_gen(message.text, file_path=f'qr_{message.from_user.id}.png')
-    photo = FSInputFile(output_file_path)
-    await message.answer_photo(photo=photo, caption=message.text)
+@dp.message(Command('get_weather'))
+async def ask_city(message: Message, state: FSMContext):
+    await state.set_state(FormCity.city)
+    await message.answer('Название города?')
 
-
-@dp.message()
+@dp.message(FormCity.city)
 async def wheather(message: Message):
     data = await get_weather(message.text)
     response_text = (f'gorod: {data.name}\n'
@@ -47,9 +65,60 @@ async def wheather(message: Message):
     await message.reply(text=response_text)
 
 
+@dp.message(Command('generate_qr'))
+async def ask_url(message: Message, state: FSMContext):
+    await state.set_state(FormUrl.url)
+    await message.answer('Укажите ссылку')
+
+
+
+
+@dp.message(FormUrl.url)
+async def generate(message: Message):
+    output_file_path = qr_gen(message.text, file_path=f'qr_{message.from_user.id}.png')
+    photo = FSInputFile(output_file_path)
+    await message.answer_photo(photo=photo, caption=message.text)
+
+
+@dp.message(Command('newpost'))
+async def start_state(message: Message, state: FSMContext):
+    await state.set_state(FormCar.marka)
+    await bot.delete_message(chat_id=message.chat.id, message_id=message.message_id)
+    await message.answer('marka?')
+
+@dp.message(FormCar.marka)
+async def set_marka(message: Message, state: FSMContext):
+    await state.update_data(marka=message.text)
+    await state.set_state(FormCar.color)
+    await message.answer('Color?')
+
+
+@dp.message(FormCar.color)
+async def set_color(message: Message, state: FSMContext):
+    await state.update_data(color=message.text)
+    await state.set_state(FormCar.year)
+    await message.answer('Year?')
+
+@dp.message(FormCar.year)
+async def set_year(message: Message, state: FSMContext):
+    await state.update_data(year=message.text)
+    await state.set_state(FormCar.country)
+    await message.answer('Country?')
+
+@dp.message(FormCar.country)
+async def set_country(message: Message, state: FSMContext):
+    await state.update_data(country=message.text)
+    await state.set_state(FormCar.gear_box)
+    await message.answer('Gear box', reply_markup=keyboard_single_row)
+
+@dp.message(FormCar.gear_box)
+async def set_gear_box(message: Message, state: FSMContext):
+    await state.update_data(gear_box=message.text)
+    data_car = await state.get_data()
+    await message.answer(f'Marka - {data_car['marka']}\nYear - {data_car['year']}\nColor - {data_car['color']}')
+
 # Run the bot
 async def main() -> None:
-    bot = Bot(token=TOKEN)
 
     await dp.start_polling(bot)
 
